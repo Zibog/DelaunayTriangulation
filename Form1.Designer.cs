608fb6cc6f86dcee83120290c64ddd77b5959b5f
Zibog <Zibog@github.com>
2021-10-25 19:31:28 +0300

    Add basic Edge class

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonTriangulate = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(760, 510);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // buttonTriangulate
+            // 
+            this.buttonTriangulate.AutoSize = true;
+            this.buttonTriangulate.Location = new System.Drawing.Point(12, 526);
+            this.buttonTriangulate.Name = "buttonTriangulate";
+            this.buttonTriangulate.Size = new System.Drawing.Size(105, 23);
+            this.buttonTriangulate.TabIndex = 1;
+            this.buttonTriangulate.Text = "Триангулировать";
+            this.buttonTriangulate.UseVisualStyleBackColor = true;
+            this.buttonTriangulate.Click += new System.EventHandler(this.buttonTriangulate_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.AutoSize = true;
+            this.buttonClear.Location = new System.Drawing.Point(123, 528);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 2;
+            this.buttonClear.Text = "Очистить";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonTriangulate);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonTriangulate;
+        private System.Windows.Forms.Button buttonClear;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -15,6 +15,56 @@ namespace DelaunayTriangulation
         public Form1()
         {
             InitializeComponent();
+            Clear();
+        }
+
+        private Bitmap map = new Bitmap(100, 100);
+        private Graphics g;
+        private List<Point> points;
+
+        private void buttonTriangulate_Click(object sender, EventArgs e)
+        {
+            var s = FindStartEdge();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            points.Add(e.Location);
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            points = new List<Point>();
+            Rectangle r = Screen.PrimaryScreen.Bounds;
+            map = new Bitmap(r.Width, r.Height);
+            g = Graphics.FromImage(map);
+            g.Clear(Color.White);
+            pictureBox1.Image = map;
+        }
+
+        private class Edge
+        {
+            public Point From;
+            public Point To;
+
+            public Point this[int i]
+            {
+                get { if (i == 0) return From; else if (i == 1) return To; else throw new ArgumentException(); }
+                set { if (i == 0) From = value; else if (i == 1) To = value; else throw new ArgumentException(); }
+            }
+        }
+
+        private Edge FindStartEdge()
+        {
+            // Берём самую левую точку
+            Point from = points.Min();
+
+            return new Edge();
         }
     }
 }
